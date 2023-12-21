a980af5cb82518e341a4de112bd9bce993af7bd8
paulandre51 <114477559+paulandre51@users.noreply.github.com>
2023-12-21 15:08:41 +0100

    add file qrcode.py

---
+++ QRcode.py
@@ -0,0 +1,20 @@
+import qrcode
+import os
+
+data = "salut toi ekip"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+downloads_path = os.path.join(os.path.expanduser('~'), 'Downloads')
+file_path = os.path.join(downloads_path, "qr_code.png")
+
+img.save(file_path)  
